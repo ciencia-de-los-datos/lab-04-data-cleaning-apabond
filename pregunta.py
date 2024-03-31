29f2f34cf3aad5a5decd8b0ec462bbd9e8d4e629
Alejandra <apabond@unal.edu.co>
2024-03-31 14:50:00 -0500

    Intento 1

--- pregunta.py
+++ pregunta.py
@@ -11,10 +11,32 @@ import pandas as pd
 
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    
 
     #
     # Inserte su código aquí
     #
 
     return df
+df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
+df.dropna(inplace=True)
+
+df["sexo"] = df["sexo"].str.lower()
+df["tipo_de_emprendimiento"] = df["tipo_de_emprendimiento"].str.lower().str.replace("-"," ").str.replace("_"," ")
+df["idea_negocio"] = df["idea_negocio"].str.lower().str.replace("-"," ").str.replace("_"," ")
+df["barrio"] = df["barrio"].str.lower().str.replace("-"," ").str.replace("_"," ")
+df["estrato"] = df["estrato"].astype("category")
+df["comuna_ciudadano"] = df["comuna_ciudadano"].astype(int)
+df["fecha_de_beneficio"] = pd.to_datetime(df["fecha_de_beneficio"], format='mixed', dayfirst=True)
+df["monto_del_credito"] = df["monto_del_credito"].str.replace(",","").str.replace(".00","").str.strip("$")
+df["monto_del_credito"] = df["monto_del_credito"].astype(int)
+df["línea_credito"] = df["línea_credito"].str.lower().str.replace("-"," ").str.replace("_"," ")
+
+df = df.drop_duplicates()
+
+
+
+
+
+
+
